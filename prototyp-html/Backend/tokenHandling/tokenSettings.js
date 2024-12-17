4ef9b1b71785753f9d320e702a6e963b1e4c575c
Amani <alawieam@hs-albsig.de>
2024-12-17 15:55:49 +0100

    montag16.12. keine ahungn

--- prototyp-html/Backend/tokenHandling/tokenSettings.js
+++ prototyp-html/Backend/tokenHandling/tokenSettings.js
@@ -1,7 +1,7 @@
 // stuff for jsonwebtoken library
 // details: https://www.npmjs.com/package/jsonwebtoken
 const algorithm = 'HS512';
-const secret = 'SUPERgeheimesPASSWORT';
+const secret = 'ichBinEineGEHEIMeBiene';
 const max_age = '1h';
 
 exports.TOKEN_ALGORITHM = algorithm;
